bbdb5c55349c27734e11c82994aea49814a98105
Elena <elena.gelzinyte@gmail.com>
2024-05-02 16:11:52 +0200

    fix A^2, option for epsilon_infinity contribution

--- util/permittivity.py
+++ util/permittivity.py
@@ -15,38 +15,32 @@ fcc_volume_density_param_N = 3.5e18 # cm^-3, page 15
 mode_data["angle"] = np.radians(mode_data["angle"])
 
 def get_rho(omega, A, f_TO, broadening):
-    return A / (f_TO**2 - omega**2 - omega * broadening * 1j)
+    return A**2 / (f_TO**2 - omega**2 - omega * broadening * 1j)
 
 
-def get_schubert(omega_range):
+def get_schubert(omega_range, epsilon_inf=True):
 
     warnings.warn("No FCC contribution to schubert permittivity")
 
     Bu = mode_data.loc[mode_data["symmetry"] == "Bu"]
     Au = mode_data.loc[mode_data["symmetry"] == "Au"]
 
-    # xx
-    eps_xx = xyz_data["high_freq"]["xx"] \
-        + np.array([np.sum(get_rho(omega, Bu["A"], Bu["freq"], Bu["scatter"]) * np.cos(Bu["angle"])**2) for omega in omega_range])
+    eps_xx =  np.array([np.sum(get_rho(omega, Bu["A"], Bu["freq"], Bu["scatter"]) * np.cos(Bu["angle"])**2) for omega in omega_range])
 
-    eps_yy = xyz_data["high_freq"]["yy"] \
-        + np.array([np.sum(get_rho(omega, Bu["A"], Bu["freq"], Bu["scatter"]) * np.sin(Bu["angle"])**2) for omega in omega_range])
+    eps_yy =  np.array([np.sum(get_rho(omega, Bu["A"], Bu["freq"], Bu["scatter"]) * np.sin(Bu["angle"])**2) for omega in omega_range])
 
-    eps_xy = xyz_data["high_freq"]["xy"] \
-        + np.array([np.sum(get_rho(omega, Bu["A"], Bu["freq"], Bu["scatter"]) * np.cos(Bu["angle"]) * np.sin(Bu["angle"])) for omega in omega_range])
+    eps_xy =  np.array([np.sum(get_rho(omega, Bu["A"], Bu["freq"], Bu["scatter"]) * np.cos(Bu["angle"]) * np.sin(Bu["angle"])) for omega in omega_range])
 
-    eps_zz = xyz_data["high_freq"]["zz"] \
-        + np.array([np.sum(get_rho(omega, Au["A"], Au["freq"], Au["scatter"])) for omega in omega_range])
+    eps_zz =  np.array([np.sum(get_rho(omega, Au["A"], Au["freq"], Au["scatter"])) for omega in omega_range])
 
 
-#     eps_xx = np.array([np.sum(get_rho(omega, Bu["A"], Bu["freq"], Bu["scatter"]) * np.cos(Bu["angle"])**2) for omega in omega_range])
-# 
-#     eps_yy = np.array([np.sum(get_rho(omega, Bu["A"], Bu["freq"], Bu["scatter"]) * np.sin(Bu["angle"])**2) for omega in omega_range])
-# 
-#     eps_xy = np.array([np.sum(get_rho(omega, Bu["A"], Bu["freq"], Bu["scatter"]) * np.cos(Bu["angle"]) * np.sin(Bu["angle"])) for omega in omega_range])
-# 
-#     eps_zz = np.array([np.sum(get_rho(omega, Au["A"], Au["freq"], Au["scatter"])) for omega in omega_range])
-# 
+    if epsilon_inf:
+        eps_xx += xyz_data["high_freq"]["xx"]
+        eps_yy += xyz_data["high_freq"]["yy"]
+        eps_zz += xyz_data["high_freq"]["zz"]
+        eps_xy += xyz_data["high_freq"]["xy"]
+
+
 
 
     data = {
@@ -54,6 +48,7 @@ def get_schubert(omega_range):
         "yy": eps_yy,
         "xy": eps_xy,
         "zz": eps_zz}
+
 
     return data
 
